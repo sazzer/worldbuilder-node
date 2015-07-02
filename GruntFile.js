546119e6138039b1cb64cc1f53359edffb9fe80e
Graham Cox <graham.cox@oclc.org>
2015-07-02 12:30:10 +0100

    Added in support for Babel for ES6 code

--- GruntFile.js
+++ GruntFile.js
@@ -1,6 +1,7 @@
 var path = require('path');
 
 var targetDir = 'target';
+var targetMainDir = path.join(targetDir, 'main');
 
 module.exports = function(grunt) {
     require('jit-grunt')(grunt, {
@@ -13,11 +14,38 @@ module.exports = function(grunt) {
             build: {
                 src: targetDir
             }
+        },
+        env: {
+            server: {
+                NODE_PATH: targetMainDir
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                optional: 'runtime'
+            },
+            server: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/server/main',
+                    src: ['**/*.js'],
+                    dest: targetMainDir
+                }]
+            }
+        },
+        execute: {
+            server: {
+                src: path.join(targetMainDir, 'main.js'),
+                options: {
+                    cwd: targetMainDir
+                }
+            }
         }
     });
 
-    grunt.registerTask('build', []);
-    grunt.registerTask('run', ['build']);
+    grunt.registerTask('build', ['babel:server']);
+    grunt.registerTask('run', ['build', 'env:server', 'execute:server']);
     grunt.registerTask('test', ['build']);
 
     grunt.registerTask('default', ['test']);
