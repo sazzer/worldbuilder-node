b0ae1606b3ac8a4733271aadfb68e411fda50b36
Graham Cox <graham@grahamcox.co.uk>
2015-07-05 15:35:04 +0100

    Seneca As Promised now acts as an alternative for calling seneca.act, instead of wrapping the seneca object

--- src/server/main/seneca/promised.js
+++ src/server/main/seneca/promised.js
@@ -1,20 +1,17 @@
 /**
  * Seneca Extension to allow actions to be performed as promises
  * @param {Seneca} seneca the Seneca instance to extend
- * @return {Seneca} the extended Seneca instance
+ * @param {Object} action The action to perform
+ * @return {Promise} for the response of the action
  */
-export function senecaAsPromised(seneca) {
-    seneca.actAsPromised = (action) => {
-        return new Promise((resolve, reject) => {
-            seneca.act(action, (error, result) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(result);
-                }
-            });
+export function senecaAsPromised(seneca, action) {
+    return new Promise((resolve, reject) => {
+        seneca.act(action, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(result);
+            }
         });
-    };
-
-    return seneca;
+    });
 }
--- src/server/test/debugActionsIntegrationTest.js
+++ src/server/test/debugActionsIntegrationTest.js
@@ -3,12 +3,15 @@ import {expect} from "chai";
 import {senecaAsPromised} from "seneca/promised";
 
 describe("seneca debug actions", () => {
-    const seneca = senecaAsPromised(senecaApp());
+    const seneca = senecaApp();
     seneca.use("debug/actions");
 
     describe("cmd:ping", () => {
         it("responds correctly with no parameters", () => {
-            return seneca.actAsPromised({role: "debug", cmd: "ping"}).then((result) => {
+            return senecaAsPromised(seneca, {
+                role: "debug",
+                cmd: "ping"
+            }).then((result) => {
                 expect(result).to.not.be.null;
                 expect(result).to.have.property("role", "debug");
                 expect(result).to.have.property("cmd", "ping");
