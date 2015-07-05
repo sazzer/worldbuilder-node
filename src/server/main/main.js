2c661a2ef7c70b91cb7526132c7a0f662a929019
Graham Cox <graham@grahamcox.co.uk>
2015-07-05 14:17:05 +0100

    Support for better stack traces based on source maps

--- src/server/main/main.js
+++ src/server/main/main.js
@@ -1,6 +1,9 @@
 import senecaApp from "seneca";
 import express from "express";
 import bodyParser from "body-parser";
+import sourceMapSupport from "source-map-support";
+
+sourceMapSupport.install();
 
 // Firstly, set up Seneca for all of the actual microservices
 const seneca = senecaApp();
