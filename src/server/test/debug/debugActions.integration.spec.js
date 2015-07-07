551cd7e3e3402e7620ed02f1a1167c84d35d7e9e
Graham Cox <graham.cox@oclc.org>
2015-07-07 12:50:56 +0100

    Test the Debug Controller correctly returns the correct time

--- src/server/test/debug/debugActions.integration.spec.js
+++ src/server/test/debug/debugActions.integration.spec.js
@@ -1,14 +1,22 @@
 /* eslint-env mocha */
 import senecaApp from "seneca";
 import {expect} from "chai";
+import moment from "moment-timezone";
+import proxyquire from "proxyquire";
 import {senecaAsPromised} from "seneca/promised";
-import debugActions from "debug/actions";
 
 describe("seneca debug actions", () => {
-    describe("cmd:ping", () => {
-        const seneca = senecaApp();
-        seneca.use(debugActions);
+    const CURRENT_TIME = "2015-07-07T11:41:54+00:00";
+    const now = moment(CURRENT_TIME);
+    const momentStub = () => now;
+    const debugActions = proxyquire("debug/actions", {
+        "moment-timezone": momentStub
+    });
 
+    const seneca = senecaApp();
+    seneca.use(debugActions);
+
+    describe("cmd:ping", () => {
         describe("when called with no parameters", () => {
             const result = senecaAsPromised(seneca, {
                 role: "debug",
@@ -65,9 +73,6 @@ describe("seneca debug actions", () => {
     });
 
     describe("cmd:now", () => {
-        const seneca = senecaApp();
-        seneca.use(debugActions);
-
         describe("when called with no parameters", () => {
             const result = senecaAsPromised(seneca, {
                 role: "debug",
@@ -83,6 +88,28 @@ describe("seneca debug actions", () => {
             it("Has the correct time", (done) => {
                 expect(result).to.eventually.have.property("now")
                     .that.is.a("string")
+                    .that.equals(CURRENT_TIME)
+                    .notify(done);
+            });
+        });
+
+        describe("when called with an alternative timezone", () => {
+            const result = senecaAsPromised(seneca, {
+                role: "debug",
+                cmd: "now",
+                tz: "America/New_York"
+            });
+
+            it("Has the correct timezone", (done) => {
+                expect(result).to.eventually.have.property("tz")
+                    .that.is.a("string")
+                    .that.equals("America/New_York")
+                    .notify(done);
+            });
+            it("Has the correct time", (done) => {
+                expect(result).to.eventually.have.property("now")
+                    .that.is.a("string")
+                    .that.equals(moment(CURRENT_TIME).tz("America/New_York").format())
                     .notify(done);
             });
         });
