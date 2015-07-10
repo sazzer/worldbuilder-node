34e5438300077e550b0fc690a74c4ae84df928cf
Graham Cox <graham.cox@oclc.org>
2015-07-10 12:35:53 +0100

    Some restructuring of things

---
+++ src/main/server/express/routes.js
@@ -0,0 +1,8 @@
+import {configureRoutes as configureDebugRoutes} from "debug/routes";
+
+/**
+ * The list of all the route configurations to support
+ */
+export const routes = [
+    configureDebugRoutes
+];
--- src/main/server/main.js
+++ src/main/server/main.js
@@ -1,45 +1,8 @@
 import sourceMapSupport from "source-map-support";
-import path from "path";
-
-import express from "express";
-import bodyParser from "body-parser";
-import compression from "compression";
-import staticFile from "connect-static-file";
-import morgan from "morgan";
-import responseTime from "response-time";
-import errorHandler from "errorhandler";
-
-import {configureRoutes as configureDebugRoutes} from "debug/routes";
+import {configureExpress} from "express/express";
+import {routes} from "express/routes";
 
 sourceMapSupport.install();
 
-// Now set up Express to handle the HTTP side of things
-const app = express();
-
-app.use(compression());
-app.use(bodyParser.json());
-app.use(responseTime());
-app.use(errorHandler());
-app.use(morgan("combined"));
-
-// Serve up the Static resources
-app.use("/api/schema", staticFile(path.join(__dirname, "schema", "schema.json"), {
-    etag: true,
-    lastModified: true
-}));
-
-const router = express.Router({
-    caseSensitive: true,
-    mergeParams: false,
-    strict: false
-});
-
-router.get("/worlds", (req, res) => {
-    res.send({hello: "world"});
-});
-
-configureDebugRoutes(router);
-
-app.use("/api", router);
-
+const app = configureExpress(routes);
 app.listen(3000);
