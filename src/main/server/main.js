0291ef235f597ce96cb388396cb418138a791b00
Graham Cox <graham@grahamcox.co.uk>
2015-07-10 07:31:20 +0100

    Threw away Seneca for now, in order to replace it with pure Express

--- src/main/server/main.js
+++ src/main/server/main.js
@@ -1,4 +1,3 @@
-import senecaApp from "seneca";
 import express from "express";
 import bodyParser from "body-parser";
 import sourceMapSupport from "source-map-support";
@@ -6,14 +5,6 @@ import staticFile from "connect-static-file";
 import path from "path";
 
 sourceMapSupport.install();
-
-// Firstly, set up Seneca for all of the actual microservices
-const seneca = senecaApp();
-seneca.use("accessToken/service")
-    .use("debug/actions", {root: "/api"})
-    .use("worlds/dal")
-    .use("worlds/service")
-    .use("worlds/rest", {root: "/api"});
 
 // Now set up Express to handle the HTTP side of things
 const app = express();
@@ -25,8 +16,5 @@ app.use("/api/schema", staticFile(path.join(__dirname, "schema", "schema.json"),
     etag: true,
     lastModified: true
 }));
-
-// And then set up Seneca to export a number of actions over HTTP
-app.use(seneca.export("web"));
 
 app.listen(3000);
