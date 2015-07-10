0136c245335a18fed34dd6ca143eafbee5985cd1
Graham Cox <graham@grahamcox.co.uk>
2015-07-10 07:45:06 +0100

    Set up lots of middleware on Express

--- src/main/server/main.js
+++ src/main/server/main.js
@@ -1,15 +1,24 @@
+import sourceMapSupport from "source-map-support";
+import path from "path";
+
 import express from "express";
 import bodyParser from "body-parser";
-import sourceMapSupport from "source-map-support";
+import compression from "compression";
 import staticFile from "connect-static-file";
-import path from "path";
+import morgan from "morgan";
+import responseTime from "response-time";
+import errorHandler from "errorhandler";
 
 sourceMapSupport.install();
 
 // Now set up Express to handle the HTTP side of things
 const app = express();
 
+app.use(compression());
 app.use(bodyParser.json());
+app.use(responseTime());
+app.use(errorHandler());
+app.use(morgan("combined"));
 
 // Serve up the Static resources
 app.use("/api/schema", staticFile(path.join(__dirname, "schema", "schema.json"), {
